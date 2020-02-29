262f5ae06cf664743de1854bf392d14dcfd06176
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2020-02-29 00:00:40 -0600

    Add profile page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,9 @@ const routes = app => {
   /** @region_snippet_routeIndex */
   app.get('/', (request, response) => response.render('index.hbs'));
 
+  /** @regions_snippet_routeProfile */
+  app.get('/profile', (request, response) => response.render('profile.hbs'));
+
   /** @regions_snippet_routeNotFound */
   app.get('*', (request, response) => response.render('404.hbs'));
 };
